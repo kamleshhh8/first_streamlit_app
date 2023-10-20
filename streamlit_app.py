6a687ef0358c282caf6e6ac2b6e9bdbb336840cb
kamleshhh8 <122210269+kamleshhh8@users.noreply.github.com>
2023-10-20 10:52:37 +0530

    Update streamlit_app.py
    emoji bug fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
-🥣 🥗 🐔 🥑🍞
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
